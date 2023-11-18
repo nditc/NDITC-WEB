fa69b466054d1f75664bc0712b273e74e3c02c1f
RafsanAmin <rafsanamin2020@gmail.com>
2023-11-18 13:41:26 +0600

    up

---
+++ src/app/Components/executive/Modal.tsx
@@ -0,0 +1,110 @@
+/* eslint-disable @next/next/no-img-element */
+import React, { Dispatch, SetStateAction, useEffect } from 'react';
+import ExecutiveData from '../../db/executives';
+
+const Field = ({ title, desc }: { title: string; desc: React.ReactNode }) => {
+  return (
+    <div>
+      <p className="font-bold text-2xl">{title}</p>
+      <p className="text-xl mt-1">{desc}</p>
+    </div>
+  );
+};
+const Links = ({ img, href }: { img: string; href: string }) => {
+  return (
+    <a
+      className="w-[30px] h-[30px] hover:fill-blue-500 transition-opacity opacity-70 hover:opacity-100"
+      href={href}
+    >
+      <img src={'/image/links/' + img + '.svg'} alt="" />
+    </a>
+  );
+};
+const Modal = ({
+  modalState,
+  setModalState,
+}: {
+  modalState: [number, number] | null;
+  setModalState: Dispatch<SetStateAction<[number, number] | null>>;
+}) => {
+  const data = modalState ? ExecutiveData.sessions[modalState[0]].members[modalState[1]] : null;
+  const firstNameArr = data?.name.split(' ');
+  const lastName = firstNameArr?.pop();
+  const firstName = firstNameArr?.join(' ') + ' ';
+  const deptName = data?.dept?.replace('Department of ', '');
+  useEffect(() => {
+    if (modalState) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+  }, [modalState]);
+  return (
+    <>
+      <div
+        className={
+          'fixed inset-0 w-full h-full bg-black  z-[60] transition-transform ' +
+          (modalState ? 'opacity-20' : 'opacity-0 pointer-events-none')
+        }
+      ></div>
+      <div
+        className={
+          'fixed left-1/2 top-1/2 translate-y-[-50%] translate-x-[-50%] w-4/5 h-4/5 rounded-2xl  bg-white z-[70] shadow-2xl transition' +
+          ' ' +
+          (modalState ? 'scale-1' : 'scale-0 pointer-events-none')
+        }
+      >
+        <img
+          className="absolute left-0 bottom-0 -z-10 min-w-[55%]"
+          src="/image/modalbg.svg"
+          alt=""
+        />
+        <button
+          className="absolute top-5 right-5 p-3 bg-slate-50 rounded-full hover:bg-blue-50  hover:fill-blue-500 transition"
+          onClick={() => setModalState(null)}
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" className="w-4" viewBox="0 0 384 512">
+            <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+          </svg>
+        </button>
+        {modalState && data ? (
+          <div className="flex align-bottom h-full">
+            <img className="" src="/image/vaiya.png" alt="" />
+            {/* <p>{JSON.stringify(ExecutiveData.sessions[modalState[0]].members[modalState[1]])}</p> */}
+            <div className="w-full pr-8 pb-8 flex flex-col justify-between h-full">
+              <div className="mt-36">
+                <h1 className="text-6xl">
+                  {firstName}
+                  <span className="text-blue-500">{lastName}</span>
+                </h1>
+                <p className="text-3xl text-blue-500 font-bold">{data.post}</p>
+                {data.dept ? (
+                  <p className="text-2xl">
+                    Department of <span className="text-blue-500">{deptName}</span>
+                  </p>
+                ) : null}
+              </div>
+              <div className="text-right flex flex-col gap-3 justify-right align-bottom">
+                <Field title="Academic Life" desc="X School > Notre Dame College > X University" />
+                <Field title="Interest" desc="Graphics Design" />
+                <Field title="E-mail" desc="random@gmail.com" />
+                <Field
+                  title="Socials"
+                  desc={
+                    <div className="flex justify-end gap-2">
+                      <Links img="fb" href="www.facebook.com" />
+                      <Links img="dribble" href="www.facebook.com" />
+                      <Links img="github" href="www.facebook.com" />
+                    </div>
+                  }
+                />
+              </div>
+            </div>
+          </div>
+        ) : null}
+      </div>
+    </>
+  );
+};
+
+export default Modal;
--- src/app/Components/executive/executives.tsx
+++ src/app/Components/executive/executives.tsx
@@ -2,22 +2,28 @@
 /* eslint-disable @next/next/no-img-element */
 'use client';
 
-import React from 'react';
+import React, { Dispatch, SetStateAction } from 'react';
+
 import ExecutiveData from '../../db/executives';
-import Image from 'next/image';
-import Members from './members';
-const Executives = () => {
+import Modal from '../../Components/Executive/Modal';
+import Member from './Member';
+
+const Executives = ({
+  setModalState,
+}: {
+  setModalState: Dispatch<SetStateAction<[number, number] | null>>;
+}) => {
   const [state, setState] = React.useState<number>(0);
   const [isOpen, setOpen] = React.useState<boolean>(false);
   return (
-    <div className="flex mt-10 flex-wrap">
+    <div className="flex pt-10 flex-wrap">
       <div className={'sticky top-24 md:w-auto w-full md:h-[20vw] h-[48px] z-10 mb-10'}>
-        <div className={'date-picker' + ' ' + (isOpen ? 'open' : 'close')}>
+        <div className={'session-selector' + ' ' + (isOpen ? 'open' : 'close')}>
           {ExecutiveData.sessions.map(({ session }, index) => {
             return (
               <div
                 className={
-                  'p-3 font-bold relative text-center rounded-xl inline-block cursor-pointer min-w-[135px] list hover:text-blue-500 ' +
+                  'p-3 font-bold relative text-center md:rounded-xl inline-block cursor-pointer min-w-[135px] session-list-item hover:text-blue-500 hover:bg-blue-50 ' +
                   (state === index ? 'bg-white text-blue-500 shadow-lg active-elem' : '')
                 }
                 key={session}
@@ -41,8 +47,10 @@ const Executives = () => {
         </div>
       </div>
       <div className="grid-design grow md:pl-10">
-        {ExecutiveData.sessions[state].members.map(({ image_url, name, post, dept }) => (
-          <Members key="" img={image_url} name={name} designation={post} department={dept} />
+        {ExecutiveData.sessions[state].members.map(({ image_url, name, post, dept }, index) => (
+          <div key="" onClick={() => setModalState([state, index])}>
+            <Member img={image_url} name={name} designation={post} department={dept} />
+          </div>
         ))}
       </div>
     </div>
