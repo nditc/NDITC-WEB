951a296fd729359bab32c4959a12dbec4c8f28bc
The-XENO-Studios <thexenostudios@gmail.com>
2023-12-14 00:07:24 +0600

    Response Changed To NextResponse

--- src/app/api/description/[url]/route.ts
+++ src/app/api/description/[url]/route.ts
@@ -1,14 +1,16 @@
 //import { useSearchParams } from "next/navigation";
 
+import { NextResponse } from "next/server";
+
 export const GET = async (
   req: Request,
   { params }: { params: { url: string } }
 ) => {
   const res = await fetch(params.url || "");
   if (!res.ok) {
-    return Response.json({ description: "No Description", notFound: true });
+    return NextResponse.json({ description: "No Description", notFound: true });
   } else {
     const data = await res.json();
-    return Response.json(data);
+    return NextResponse.json(data);
   }
 };
