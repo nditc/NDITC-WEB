e3c9c67f35a6a1c581dff6fc3db870ddcf375f8d
The-XENO-Studios <thexenostudios@gmail.com>
2023-12-14 21:57:59 +0600

    Skeleton Loading Text Added

--- src/app/activities/loading.tsx
+++ src/app/activities/loading.tsx
@@ -62,7 +62,44 @@ const loading = () => {
                       <h1 className="text-2xl min-h-[64px] grid place-items-center">
                         Loading...
                       </h1>
-                      <p className="line-clamp-5">{"Won't wait for long :)"}</p>
+                      <p className="line-clamp-5">
+                        <div
+                          role="status"
+                          className="space-y-2.5 animate-pulse max-w-lg"
+                        >
+                          <div className="flex items-center w-full">
+                            <div className="h-2.5 bg-gray-400 rounded-full w-32"></div>
+                            <div className="h-2.5 ms-2 bg-gray-500 rounded-full w-24"></div>
+                            <div className="h-2.5 ms-2 bg-gray-500 rounded-full w-full"></div>
+                          </div>
+                          <div className="flex items-center w-full max-w-[480px]">
+                            <div className="h-2.5 bg-gray-400 rounded-full w-full"></div>
+                            <div className="h-2.5 ms-2 bg-gray-500 rounded-full w-full"></div>
+                            <div className="h-2.5 ms-2 bg-gray-500 rounded-full w-24"></div>
+                          </div>
+                          <div className="flex items-center w-full max-w-[400px]">
+                            <div className="h-2.5 bg-gray-400 rounded-full w-full"></div>
+                            <div className="h-2.5 ms-2 bg-gray-400 rounded-full w-80"></div>
+                            <div className="h-2.5 ms-2 bg-gray-500 rounded-full w-full"></div>
+                          </div>
+                          <div className="flex items-center w-full max-w-[480px]">
+                            <div className="h-2.5 ms-2 bg-gray-400 rounded-full w-full"></div>
+                            <div className="h-2.5 ms-2 bg-gray-500 rounded-full w-full"></div>
+                            <div className="h-2.5 ms-2 bg-gray-500 rounded-full w-24"></div>
+                          </div>
+                          <div className="flex items-center w-full max-w-[440px]">
+                            <div className="h-2.5 ms-2 bg-gray-500 rounded-full w-32"></div>
+                            <div className="h-2.5 ms-2 bg-gray-500 rounded-full w-24"></div>
+                            <div className="h-2.5 ms-2 bg-gray-400 rounded-full w-full"></div>
+                          </div>
+                          <div className="flex items-center w-full max-w-[360px]">
+                            <div className="h-2.5 ms-2 bg-gray-500 rounded-full w-full"></div>
+                            <div className="h-2.5 ms-2 bg-gray-400 rounded-full w-80"></div>
+                            <div className="h-2.5 ms-2 bg-gray-500 rounded-full w-full"></div>
+                          </div>
+                          <span className="sr-only">Loading...</span>
+                        </div>
+                      </p>
                     </div>
                     <div className="w-full grid place-items-center justify-self-end">
                       <div className="learn_more text-white text-lg cursor-pointer">
