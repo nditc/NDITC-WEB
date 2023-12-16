8d9f1ec4aea65316058a708a2aa46ca00ac62101
The-XENO-Studios <thexenostudios@gmail.com>
2023-12-16 21:01:00 +0600

    Upcoming Event String Fixed

--- src/app/activities/page.tsx
+++ src/app/activities/page.tsx
@@ -1,11 +1,11 @@
-import CodeCompass from '../Components/CodeCompass';
-import EventsList from './EventsList';
-import { useSearchParams } from 'next/navigation';
+import CodeCompass from "../Components/CodeCompass";
+import EventsList from "./EventsList";
+import { useSearchParams } from "next/navigation";
 
 type Events = {
   data: [
     {
-      category: 'event' | 'workshop';
+      category: "event" | "workshop";
       details_url: string;
       image_url: string;
       subtitle: string;
@@ -17,17 +17,17 @@ type Events = {
 
 const getEventData = async (type: string) => {
   let modifiedType;
-  if (type == 'projects' || type == 'publications') {
+  if (type == "projects" || type == "publications") {
     modifiedType = type;
   } else {
-    modifiedType = 'activities/' + type;
+    modifiedType = "activities/" + type;
   }
-  const res = await fetch('https://nditc.pythonanywhere.com/' + modifiedType, {
-    cache: 'no-store',
+  const res = await fetch("https://nditc.pythonanywhere.com/" + modifiedType, {
+    cache: "no-store",
   });
 
   if (!res.ok) {
-    console.error('Problem Occurred');
+    console.error("Problem Occurred");
     return [];
   }
   return res.json();
@@ -39,11 +39,15 @@ const Activities = async ({
   params: {};
   searchParams: { [key: string]: string | string[] | undefined };
 }) => {
-  const events = await getEventData(searchParams?.type + 's');
-  const upcoming = await getEventData('upcoming');
+  const events = await getEventData(searchParams?.type + "s");
+  const upcoming = await getEventData("upcoming");
   return (
     <div className="w-screen bg-[#F6F6F6]">
-      <img src="/image/bg2.svg" className="absolute top-1/4 right-0 z-0" alt=""></img>
+      <img
+        src="/image/bg2.svg"
+        className="absolute top-1/4 right-0 z-0"
+        alt=""
+      ></img>
       <div className="container pt-32 py-10  flex flex-col items-center gap-10 z-10 bg-transparent relative">
         <div className="flex gap-3 items-end justify-center self-start md:justify-start">
           <h1 className="text-3xl md:text-5xl pb-1">UPCOMING</h1>
@@ -51,7 +55,9 @@ const Activities = async ({
         </div>
         <section className="w-full ">
           <div className="w-full grid place-items-center h-60 bg-slate-400 rounded-xl text-slate-100 shadow-xl ">
-            <p className="Bebas text-2xl">{JSON.stringify(upcoming)}</p>
+            <p className="Bebas text-2xl overflow-hidden">
+              {JSON.stringify(upcoming)}
+            </p>
           </div>
         </section>
         <EventsList data={events} />
