96bcdb6b0c0287091a2eb60b953fd3a34e428468
The-XENO-Studios <thexenostudios@gmail.com>
2023-12-21 15:54:12 +0600

    About Us Text Added

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,88 +1,62 @@
-import CommonPage from '../Components/CommonPage/CommonPage';
-import { FaRegQuestionCircle } from 'react-icons/fa';
+import CommonPage from "../Components/CommonPage/CommonPage";
+import { FaRegQuestionCircle } from "react-icons/fa";
 
 const Sections = [
   {
-    heading: 'Heading1',
+    heading: "About Us",
     content: (
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quas laborum iure vero
-        enim sequi temporibus, repellendus ipsum, animi ex quidem, officiis alias nostrum explicabo
-        odio doloremque. Iure id hic quo iusto delectus nemo consequatur aut est qui doloremque,
-        voluptatum soluta voluptate architecto aperiam repellat ex, molestiae veniam pariatur at!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum cupiditate quo vel veniam
-        fuga omnis aperiam deleniti, ipsum voluptate odit, animi et quos? Nihil ad facere voluptatum
-        vero placeat dolores quia voluptates amet labore quisquam reiciendis tempora eum nisi
-        deserunt, earum doloribus odio fuga consequuntur explicabo similique! Amet illum delectus
-        aliquam nihil impedit officiis laudantium, id aut deserunt aperiam ducimus eum similique
-        esse! A amet ex voluptatibus quaerat ullam ea non quidem nulla modi earum tempora
-        perspiciatis voluptatem, recusandae totam eaque id eos aspernatur! Asperiores non, ex
-        voluptas illum molestias quia, laboriosam enim, sunt eveniet tempora eaque ipsum aperiam
-        voluptatem voluptate unde natus assumenda officiis? Quod architecto maiores quis dolorem
-        nesciunt deleniti hic natus illum corporis, sint officiis totam eius quos dicta consequuntur
-        doloribus dolor animi, vero delectus iusto soluta dolore aperiam ipsa. Pariatur inventore,
-        soluta magni ut, est dolorem suscipit quod id doloremque iure sapiente, adipisci corporis
-        molestiae ipsam eos. Voluptate ut placeat, quaerat culpa praesentium iusto eos explicabo
-        fuga? Quidem similique blanditiis eveniet nostrum quod itaque alias adipisci maxime illum
-        nihil. Facere, cumque dolor sed ducimus et tempore ratione delectus voluptas dignissimos
-        maiores repudiandae? Distinctio officiis porro modi nisi ipsum hic cupiditate culpa? Quis
-        eveniet natus odit? Corrupti.
+        Notre Dame Information Technology Club (NDITC), a co-curricular
+        activities club of the prestigious Notre Dame College, has started its
+        journey on 2018, to promote and support learning and interaction within
+        areas related to information systems and technology, to share new ideas
+        and to encourage recreational activities in these areas. "Innovate and
+        Encode Your Ideas" bearing this motto, the club has been established.
+        Being the first club of Notre Dame College devoted to encourage the
+        students to take part and make contribution to the ever evolving IT
+        sector, NDITC is dedicated to set an example. The club aspires to
+        encourage its members to discover their untapped zeal for information
+        and technology. NDITC aims to give the necessary guidelines and
+        opportunity and act as the leading light in spreading technological
+        awareness among the pupils. Information and technology, with their own
+        conundrum, are expected to create students who are up-to-date with the
+        current world, being led by this sincere institution of knowledge. The
+        club has taken the solemn oath to uphold the rich history of Notre Dame
+        College through our activities and ignite the thirst for knowledge about
+        the technology world within the students which, we believe, will
+        construct the pathway towards a technologically advanced nation.
       </p>
     ),
   },
   {
-    heading: 'Heading2',
+    heading: "About Our Motive",
     content: (
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quas laborum iure vero
-        enim sequi temporibus, repellendus ipsum, animi ex quidem, officiis alias nostrum explicabo
-        odio doloremque. Iure id hic quo iusto delectus nemo consequatur aut est qui doloremque,
-        voluptatum soluta voluptate architecto aperiam repellat ex, molestiae veniam pariatur at!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum cupiditate quo vel veniam
-        fuga omnis aperiam deleniti, ipsum voluptate odit, animi et quos? Nihil ad facere voluptatum
-        vero placeat dolores quia voluptates amet labore quisquam reiciendis tempora eum nisi
-        deserunt, earum doloribus odio fuga consequuntur explicabo similique! Amet illum delectus
-        aliquam nihil impedit officiis laudantium, id aut deserunt aperiam ducimus eum similique
-        esse! A amet ex voluptatibus quaerat ullam ea non quidem nulla modi earum tempora
-        perspiciatis voluptatem, recusandae totam eaque id eos aspernatur! Asperiores non, ex
-        voluptas illum molestias quia, laboriosam enim, sunt eveniet tempora eaque ipsum aperiam
-        voluptatem voluptate unde natus assumenda officiis? Quod architecto maiores quis dolorem
-        nesciunt deleniti hic natus illum corporis, sint officiis totam eius quos dicta consequuntur
-        doloribus dolor animi, vero delectus iusto soluta dolore aperiam ipsa. Pariatur inventore,
-        soluta magni ut, est dolorem suscipit quod id doloremque iure sapiente, adipisci corporis
-        molestiae ipsam eos. Voluptate ut placeat, quaerat culpa praesentium iusto eos explicabo
-        fuga? Quidem similique blanditiis eveniet nostrum quod itaque alias adipisci maxime illum
-        nihil. Facere, cumque dolor sed ducimus et tempore ratione delectus voluptas dignissimos
-        maiores repudiandae? Distinctio officiis porro modi nisi ipsum hic cupiditate culpa? Quis
-        eveniet natus odit? Corrupti.
-      </p>
-    ),
-  },
-  {
-    heading: 'Heading2',
-    content: (
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quas laborum iure vero
-        enim sequi temporibus, repellendus ipsum, animi ex quidem, officiis alias nostrum explicabo
-        odio doloremque. Iure id hic quo iusto delectus nemo consequatur aut est qui doloremque,
-        voluptatum soluta voluptate architecto aperiam repellat ex, molestiae veniam pariatur at!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum cupiditate quo vel veniam
-        fuga omnis aperiam deleniti, ipsum voluptate odit, animi et quos? Nihil ad facere voluptatum
-        vero placeat dolores quia voluptates amet labore quisquam reiciendis tempora eum nisi
-        deserunt, earum doloribus odio fuga consequuntur explicabo similique! Amet illum delectus
-        aliquam nihil impedit officiis laudantium, id aut deserunt aperiam ducimus eum similique
-        esse! A amet ex voluptatibus quaerat ullam ea non quidem nulla modi earum tempora
-        perspiciatis voluptatem, recusandae totam eaque id eos aspernatur! Asperiores non, ex
-        voluptas illum molestias quia, laboriosam enim, sunt eveniet tempora eaque ipsum aperiam
-        voluptatem voluptate unde natus assumenda officiis? Quod architecto maiores quis dolorem
-        nesciunt deleniti hic natus illum corporis, sint officiis totam eius quos dicta consequuntur
-        doloribus dolor animi, vero delectus iusto soluta dolore aperiam ipsa. Pariatur inventore,
-        soluta magni ut, est dolorem suscipit quod id doloremque iure sapiente, adipisci corporis
-        molestiae ipsam eos. Voluptate ut placeat, quaerat culpa praesentium iusto eos explicabo
-        fuga? Quidem similique blanditiis eveniet nostrum quod itaque alias adipisci maxime illum
-        nihil. Facere, cumque dolor sed ducimus et tempore ratione delectus voluptas dignissimos
-        maiores repudiandae? Distinctio officiis porro modi nisi ipsum hic cupiditate culpa? Quis
-        eveniet natus odit? Corrupti.
+        Technology, the catalyst of modern human revolution, has rooted its
+        megabytes, gigabytes and terabytes into every neuron and synapse of our
+        human brains, conquering the whole world with its enigma in the process.
+        In this age, where information has become synonymous to power; with
+        technology as its driving force, the need for a club in Notre Dame
+        College for the young tech enthusiasts couldn't be more. You asked for
+        it. We made it happen. Notre Dame Information Technology Club (NDITC), a
+        co-curricular activities club of the prestigious Notre Dame College, has
+        started its journey on 2018, to promote and support learning and
+        interaction within areas related to information systems and technology,
+        to share new ideas and to encourage recreational activities in these
+        areas. "Innovate and Encode Your Ideas" bearing this motto, the club has
+        been established. Being the first club of Notre Dame College devoted to
+        encourage the students to take part and make contribution to the ever
+        evolving IT sector, NDITC is dedicated to set an example. The club
+        aspires to encourage its members to discover their untapped zeal for
+        information and technology. NDITC aims to give the necessary guidelines
+        and opportunity and act as the leading light in spreading technological
+        awareness among the pupils. Information and technology, with their own
+        conundrum, are expected to create students who are up-to-date with the
+        current world, being led by this sincere institution of knowledge. The
+        club has taken the solemn oath to uphold the rich history of Notre Dame
+        College through our activities and ignite the thirst for knowledge about
+        the technology world within the students which, we believe, will
+        construct the pathway towards a technologically advanced nation.
       </p>
     ),
   },
@@ -93,7 +67,7 @@ const About = () => {
     <div>
       <CommonPage
         heading="About Us"
-        icon={<FaRegQuestionCircle className={'w-16 h-16'} />}
+        icon={<FaRegQuestionCircle className={"w-16 h-16"} />}
         sections={Sections}
       />
     </div>
