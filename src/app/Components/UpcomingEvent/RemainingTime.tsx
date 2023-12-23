7dfc10976d6243eed0ac0c0fbbccadb874d763be
The-XENO-Studios <thexenostudios@gmail.com>
2023-12-23 14:41:20 +0600

    Upcoming Event Tweaks

--- src/app/Components/UpcomingEvent/RemainingTime.tsx
+++ src/app/Components/UpcomingEvent/RemainingTime.tsx
@@ -33,16 +33,16 @@ const RemainingTime = ({ time }: { time: number }) => {
   return (
     <div className="right-part pt-5">
       <div className="circle1">
-        <div className="number">{timeLeft.days}</div>
-        <div className="word">Days</div>
+        <div className="number text-white">{timeLeft.days}</div>
+        <div className="word text-white mix-blend-difference">Days</div>
       </div>
       <div className="circle2">
-        <div className="number">{timeLeft.minutes}</div>
-        <div className="word">Minutes</div>
+        <div className="number text-white">{timeLeft.minutes}</div>
+        <div className="word text-white mix-blend-difference">Minutes</div>
       </div>
       <div className="circle3">
-        <span className="number">{timeLeft.seconds}</span>
-        <span className="word">Seconds</span>
+        <span className="number text-white">{timeLeft.seconds}</span>
+        <span className="word text-white mix-blend-difference">Seconds</span>
       </div>
     </div>
   );
