c91c8fcdb6b319f3ed692cb2493d7deb0357ae6b
The-XENO-Studios <thexenostudios@gmail.com>
2023-12-24 20:16:55 +0600

    Dev Page Updated

--- src/app/developer/page.tsx
+++ src/app/developer/page.tsx
@@ -12,7 +12,44 @@ const Developer = () => {
       </div>
 
       <div className="my-12 flex flex-col md:flex-row gap-3 justify-center md:justify-start items-start md:items-end">
-        <h1 className="text-4xl md:text-6xl">MAIN</h1>
+        <h1 className="text-4xl md:text-6xl">SUPERVISION AND</h1>
+        <h1 className="text-4xl md:text-6xl text-blue-500">GUIDELINES</h1>
+      </div>
+
+      <div className="flex pt-10 flex-wrap gap-56 justify-start">
+        <a href="https://www.facebook.com/ahammadshawki8/" target="_blank">
+          <Member
+            hasClickHandler={true}
+            img="https://raw.githubusercontent.com/nditc/nditc_mobile_app/main/images/executives/2022-2023/15.jpg"
+            name="Ahammad Shawki"
+            designation="Full Stack WEB Developer"
+            department="Web & App Development"
+          />
+        </a>
+
+        <a href="https://www.facebook.com/kt.prodhan" target="_blank">
+          <Member
+            hasClickHandler={true}
+            img="https://portfolio-of-nafe.web.app/assets/img/profile-img.png"
+            name="Nafe Ibne Dalower"
+            designation="Full Stack WEB Developer"
+            department="Web & App Development"
+          />
+        </a>
+
+        <a href="https://www.facebook.com/kt.prodhan" target="_blank">
+          <Member
+            hasClickHandler={true}
+            img="https://portfolio-of-nafe.web.app/assets/img/profile-img.png"
+            name="Adib Adnan"
+            designation="Full Stack WEB Developer"
+            department="Web & App Development"
+          />
+        </a>
+      </div>
+
+      <div className="mt-36 mb-10 flex flex-col md:flex-row gap-3 justify-center md:justify-start items-start md:items-end">
+        <h1 className="text-4xl md:text-6xl">FULL-STACK</h1>
         <h1 className="text-4xl md:text-6xl text-blue-500">DEVELOPERS</h1>
       </div>
 
@@ -45,54 +82,7 @@ const Developer = () => {
       </div>
 
       <div className="mt-36 mb-10 flex flex-col md:flex-row gap-3 justify-center md:justify-start items-start md:items-end">
-        <h1 className="text-4xl md:text-6xl">THE</h1>
-        <h1 className="text-4xl md:text-6xl text-blue-500">DESIGNER</h1>
-      </div>
-
-      <div className="flex pt-10 flex-wrap gap-56 justify-start">
-        <a
-          href="https://www.facebook.com/profile.php?id=100083625623282"
-          target="_blank"
-        >
-          <Member
-            hasClickHandler={true}
-            img="https://scontent.fdac142-1.fna.fbcdn.net/v/t39.30808-6/383970588_283776561086529_4495662120361222194_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=Fr03OW0sMPYAX_iCC8r&_nc_ht=scontent.fdac142-1.fna&oh=00_AfBM0jKSYfsyKxme2VO99pw_s0f7MdLEhV05Lgt26b6-cg&oe=658BD1AE"
-            name="Wasef Rahman Swapnil"
-            designation="Full Stack WEB Developer"
-            department=""
-          />
-        </a>
-      </div>
-
-      <div className="mt-36 mb-10 flex flex-col md:flex-row gap-3 justify-center md:justify-start items-start md:items-end">
-        <h1 className="text-4xl md:text-6xl">SUPERVISION AND</h1>
-        <h1 className="text-4xl md:text-6xl text-blue-500">GUIDELINES</h1>
-      </div>
-
-      <div className="flex pt-10 flex-wrap gap-56 justify-start">
-        <a href="https://www.facebook.com/ahammadshawki8/" target="_blank">
-          <Member
-            hasClickHandler={true}
-            img="https://raw.githubusercontent.com/nditc/nditc_mobile_app/main/images/executives/2022-2023/15.jpg"
-            name="Ahammad Shawki"
-            designation="Full Stack WEB Developer"
-            department="Web & App Development"
-          />
-        </a>
-
-        <a href="https://www.facebook.com/kt.prodhan" target="_blank">
-          <Member
-            hasClickHandler={true}
-            img="https://portfolio-of-nafe.web.app/assets/img/profile-img.png"
-            name="Nafe Ibne Dalower"
-            designation="Full Stack WEB Developer"
-            department="Web & App Development"
-          />
-        </a>
-      </div>
-
-      <div className="mt-36 mb-10 flex flex-col md:flex-row gap-3 justify-center md:justify-start items-start md:items-end">
-        <h1 className="text-4xl md:text-6xl">OUR</h1>
+        <h1 className="text-4xl md:text-6xl">FRONTEND</h1>
         <h1 className="text-4xl md:text-6xl text-blue-500">DEVELOPERS</h1>
       </div>
 
