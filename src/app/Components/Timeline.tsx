0b7edc437214de66b77faa5ac250051bba520b60
RafsanAmin <rafsanamin2020@gmail.com>
2024-01-07 22:58:02 +0600

    d

--- src/app/Components/Timeline.tsx
+++ src/app/Components/Timeline.tsx
@@ -1,89 +1,99 @@
-"use client";
+'use client';
 
-import { useEffect, useState } from "react";
-import { MdKeyboardArrowUp, MdKeyboardArrowDown } from "react-icons/md";
+import { useEffect, useState } from 'react';
+import { MdKeyboardArrowUp, MdKeyboardArrowDown } from 'react-icons/md';
 
 const Timeline = () => {
   const [selectedIndex, setSelectedIndex] = useState(0);
 
   const timelineData = [
     {
-      time1: "2018",
-      time2: "01 Jan",
-      title: "Our journey began",
+      time1: '2018',
+      time2: '01 Jan',
+      title: 'Our journey began',
       description:
-        "Born in 2018, NDITC sprouted at Notre Dame College to nurture tech talent and bridge the gap between knowledge and innovation.",
-      imgURL: "/image/timeline/1.jpg",
+        'Born in 2018, NDITC sprouted at Notre Dame College to nurture tech talent and bridge the gap between knowledge and innovation.',
+      imgURL: '/image/timeline/1.jpg',
     },
     {
-      time1: "2018",
-      time2: "21 Mar",
-      title: "College Robotics Contest Winner",
+      time1: '2018',
+      time2: '21 Mar',
+      title: 'College Robotics Contest Winner',
       description:
-        "Our first achievement came when we won the 1st robotics competition for college organized by campusbd.net with a price of 1 lakh taka.",
-      imgURL: "/image/timeline/Timeline.png",
+        'Our first achievement came when we won the 1st robotics competition for college organized by campusbd.net with a price of 1 lakh taka.',
+      imgURL: '/image/timeline/Timeline.png',
     },
     {
-      time1: "2018",
-      time2: "23 Nov",
-      title: "INIT 2018: Our very first national event",
+      time1: '2018',
+      time2: '23 Nov',
+      title: 'INIT 2018: Our very first national event',
       description:
-        "We successfully organized our first event ``NDITC_init 2018`` with 11 different segments.",
-      imgURL: "/image/timeline/3.jpg",
+        'We successfully organized our first event ``NDITC_init 2018`` with 11 different segments.',
+      imgURL: '/image/timeline/3.jpg',
     },
     {
-      time1: "2019",
-      time2: "09 Aug",
-      title: "Tinker, First non-human member, was born",
-      description: "The first robot of our club.",
-      imgURL: "/image/timeline/4.jpg",
+      time1: '2019',
+      time2: '09 Aug',
+      title: 'Tinker, First non-human member, was born',
+      description: 'The first robot of our club.',
+      imgURL: '/image/timeline/4.jpg',
     },
     {
-      time1: "2023",
-      time2: "03 Apr",
-      title: "Our AI project, EVYA.AI",
+      time1: '2023',
+      time2: '03 Apr',
+      title: 'Our AI project, EVYA.AI',
       description:
-        "Our R&D team developed a Face Recognition application for our AI project named ‘EVYA.AI’ using the Python programming language.",
-      imgURL: "/image/timeline/5.jpg",
+        'Our R&D team developed a Face Recognition application for our AI project named ‘EVYA.AI’ using the Python programming language.',
+      imgURL: '/image/timeline/5.jpg',
     },
     {
-      time1: "2023",
-      time2: "16 May",
-      title: "Publishing First Monthly Newsletter",
+      time1: '2023',
+      time2: '16 May',
+      title: 'Publishing First Monthly Newsletter',
       description:
-        "In order to keep ourselves updated about our club activities, trending tech stories and sharing resources, we published our first monthly newsletter ``Code Compass``.",
-      imgURL: "/image/timeline/6.jpg",
+        'In order to keep ourselves updated about our club activities, trending tech stories and sharing resources, we published our first monthly newsletter ``Code Compass``.',
+      imgURL: '/image/timeline/6.jpg',
     },
     {
-      time1: "2023",
-      time2: "15 Jun",
-      title: "Official Android app was launched",
-      description:
-        "We published an android application to attend many club purposes.",
-      imgURL: "/image/timeline/7.jpg",
+      time1: '2023',
+      time2: '15 Jun',
+      title: 'Official Android app was launched',
+      description: 'We published an android application to attend many club purposes.',
+      imgURL: '/image/timeline/7.jpg',
     },
     {
-      time1: "2023",
-      time2: "29 Nov",
-      title: "First wall magazine",
-      description:
-        "We launched our first wall magazine at the fresher's ceremony",
-      imgURL: "/image/timeline/8.jpg",
+      time1: '2023',
+      time2: '29 Nov',
+      title: 'First wall magazine',
+      description: "We launched our first wall magazine at the fresher's ceremony",
+      imgURL: '/image/timeline/8.jpg',
     },
   ];
 
   const [currentTitle, setCurrentTitle] = useState(timelineData[0].title);
 
-  const [currentDescription, setCurrentDescription] = useState(
-    timelineData[0].description
-  );
+  const [currentDescription, setCurrentDescription] = useState(timelineData[0].description);
 
   const [currentImage, setCurrentImage] = useState(timelineData[0].imgURL);
 
   const setCurrentData = (i: number) => {
     setSelectedIndex(i);
   };
+  useEffect(() => {
+    const s = setInterval(() => {
+      setSelectedIndex((s) => {
+        if (s === timelineData.length - 1) {
+          return 0;
+        } else {
+          return s + 1;
+        }
+      });
+    }, 4000);
 
+    return () => {
+      clearInterval(s);
+    };
+  }, [timelineData.length]);
   useEffect(() => {
     setCurrentTitle(timelineData[selectedIndex].title);
     setCurrentDescription(timelineData[selectedIndex].description);
@@ -91,7 +101,29 @@ const Timeline = () => {
   }, [selectedIndex]);
 
   return (
-    <section className="h-96 w-full mt-28 mb-32  flex flex-col md:flex-row justify-center">
+    <section className="w-full lg:w-0 flex-1 gap-2 md:gap-0 flex flex-col md:flex-row justify-center relative">
+      <MdKeyboardArrowUp
+        onClick={() => {
+          if (selectedIndex > 0) {
+            setSelectedIndex(selectedIndex - 1);
+          }
+        }}
+        className={`w-10 h-10 cursor-pointer absolute top-2 left-0  z-20 -rotate-90 md:left-auto md:rotate-0 md:-top-2 transition-all ${
+          selectedIndex > 0 ? 'scale-110 hover:scale-150' : 'text-gray-500 scale-90'
+        }`}
+      />
+      <MdKeyboardArrowDown
+        onClick={() => {
+          if (selectedIndex < timelineData.length - 1) {
+            setSelectedIndex(selectedIndex + 1);
+          }
+        }}
+        className={`w-10 h-10 cursor-pointer absolute top-2 md:top-[unset]  z-20  right-0 -rotate-90 md:right-auto md:rotate-0 md:-bottom-2 transition-all ${
+          selectedIndex < timelineData.length - 1
+            ? 'scale-110 hover:scale-150'
+            : 'text-gray-500 scale-90'
+        }`}
+      />
       <div className="flex flex-col md:flex-row gap-5">
         <div className="h-full flex flex-row md:flex-col flex-1 justify-center gap-5">
           {timelineData.map((e, i) => {
@@ -110,41 +142,16 @@ const Timeline = () => {
         <div className="h-1 w-full md:w-1 md:h-full bg-gray-400 rounded-3xl md:gradient-mask-t-90-d hidden md:flex" />
       </div>
 
-      <div className="flex-[10] flex justify-center items-center relative">
-        <MdKeyboardArrowUp
-          onClick={() => {
-            if (selectedIndex > 0) {
-              setSelectedIndex(selectedIndex - 1);
-            }
-          }}
-          className={`w-10 h-10 cursor-pointer absolute -left-4 -rotate-90 md:left-auto md:rotate-0 md:-top-16 transition-all ${
-            selectedIndex > 0
-              ? "scale-110 hover:scale-150"
-              : "text-gray-500 scale-90"
-          }`}
-        />
-        <MdKeyboardArrowDown
-          onClick={() => {
-            if (selectedIndex < timelineData.length - 1) {
-              setSelectedIndex(selectedIndex + 1);
-            }
-          }}
-          className={`w-10 h-10 cursor-pointer absolute -right-4 -rotate-90 md:right-auto md:rotate-0 md:-bottom-16 transition-all ${
-            selectedIndex < timelineData.length - 1
-              ? "scale-110 hover:scale-150"
-              : "text-gray-500 scale-90"
-          }`}
-        />
-
-        <div className="h-[90%] w-[90%] bg-white rounded-xl shadow-[020px_20px_20px_10px_#00000024] flex flex-col md:flex-row items-center justify-center gap-5 px-[1rem]">
+      <div className="flex-[10] my-auto  md:ml-10 h-full  flex justify-center items-center relative">
+        <div className="w-full  bg-white rounded-xl shadow-xl grid grid-rows-[1fr_280px] items-stretch justify-items-start gap-5 p-4 md:p-6">
           <div className="gap-5 flex-col">
-            <h1 className="text-3xl md:text-5xl text-left">{currentTitle}</h1>
+            <h1 className="text-3xl md:text-4xl text-left">{currentTitle}</h1>
             <p>{currentDescription}</p>
           </div>
           <img
             src={currentImage}
             alt="Image"
-            className="w-full h-[90%] rounded-[.75rem] object-cover"
+            className="w-full h-full max-h-[280px] rounded-[.75rem] object-cover"
             width={720}
             height={720}
           />
@@ -173,33 +180,29 @@ const Time = ({
 }) => {
   return (
     <div
-      className={`relative cursor-pointer ${
-        current ? "" : "hidden"
-      } md:flex mb-1 md:mb-0`}
+      className={`relative cursor-pointer ${current ? '' : 'hidden'} md:flex mb-1 md:mb-0`}
       onClick={() => {
         setCurrentAsSelected(index);
       }}
     >
-      <div className="w-10">
+      <div className="w-16">
         <h1
           className={`transition-all ${
-            current ? "text-2xl" : "text-gray-400 text-xl"
+            current ? 'text-3xl text-blue-500' : 'text-gray-400 text-2xl'
           }`}
         >
           {time1}
         </h1>
-        <h1
-          className={`transition-all ${
-            current ? "text-base" : "text-gray-400 text-sm"
-          }`}
+        <p
+          className={`transition-all  ${current ? 'text-base font-bold' : 'text-gray-400 text-sm'}`}
         >
           {time2}
-        </h1>
+        </p>
       </div>
 
       <div
-        className={`hidden transition-all duration-300 w-5 h-5 rounded-full absolute bg-blue-500 z-10 left-[50%] -translate-x-[50%] top-[4.25rem] md:translate-x-0 md:left-[3.25rem] md:top-[50%] md:-translate-y-[50%] md:flex items-center justify-center shadow-[5px_5px_20px_10px_#00000024] ${
-          current ? "scale-100" : "scale-0"
+        className={`hidden transition-all duration-300 w-5 h-5 rounded-full absolute bg-blue-500 z-10 left-[50%] -translate-x-[50%] top-[4.25rem] md:translate-x-0 md:left-[4.75rem] md:top-[50%] md:-translate-y-[50%] md:flex items-center justify-center shadow-[5px_5px_20px_10px_#00000024] ${
+          current ? 'scale-100' : 'scale-0'
         }`}
       >
         <div className="w-3 h-3 bg-white rounded-full" />
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,20 +2,56 @@ import CommonPage from '../Components/CommonPage/CommonPage';
 import { FaRegQuestionCircle } from 'react-icons/fa';
 import Timeline from '../Components/Timeline';
 import Image from 'next/image';
+import DataCard from '../Components/DataCard';
 
 const Sections = [
   {
     heading: '',
     content: (
       <>
-        <Image
-          src="/About.jpg"
-          alt="Image"
-          className="w-full rounded-xl object-cover mb-10"
-          width={810}
-          height={810}
-        />
-        <p>
+        <div className="flex gap-8 lg:flex-row flex-col  mb-10">
+          <Image
+            src="/About.jpg"
+            alt="Image"
+            className="rounded-xl object-cover w-full lg:max-w-[50%]"
+            width={1920}
+            height={1080}
+          />
+          <p className="flex-1">
+            Notre Dame Information Technology Club (NDITC), a co-curricular activities club of the
+            prestigious Notre Dame College, has started its journey on 2018, to promote and support
+            learning and interaction within areas related to information systems and technology, to
+            share new ideas and to encourage recreational activities in these areas. "Innovate and
+            Encode Your Ideas" bearing this motto, the club has been established. Being the first
+            club of Notre Dame College devoted to encourage the students to take part and make
+            contribution to the ever evolving IT sector, NDITC is dedicated to set an example. The
+            club aspires to encourage its members to discover their untapped zeal for information
+            and technology. NDITC aims to give the necessary guidelines and opportunity and act as
+            the leading light in spreading technological awareness among the pupils. Information and
+            technology, with their own conundrum, are expected to create students who are up-to-date
+            with the current world, being led by this sincere institution of knowledge. The club has
+            taken the solemn oath to uphold the rich history of Notre Dame College through our
+            activities and ignite the thirst for knowledge about the technology world within the
+            students which, we believe, will construct the pathway towards a technologically
+            advanced nation.
+          </p>
+        </div>
+        <h1 className="text-4xl md:text-5xl mb-5">
+          Our<span className="text-blue-500"> Achievments</span>
+        </h1>
+        <div className="flex flex-col lg:flex-row gap-4 md:gap-8 mb-10">
+          <Timeline />
+          <DataCard />
+        </div>
+        <h1 className="text-4xl md:text-5xl mb-3">
+          Our<span className="text-blue-500"> Motive</span>
+        </h1>
+        <p className="mb-10">
+          Technology, the catalyst of modern human revolution, has rooted its megabytes, gigabytes
+          and terabytes into every neuron and synapse of our human brains, conquering the whole
+          world with its enigma in the process. In this age, where information has become synonymous
+          to power; with technology as its driving force, the need for a club in Notre Dame College
+          for the young tech enthusiasts couldn't be more. You asked for it. We made it happen.
           Notre Dame Information Technology Club (NDITC), a co-curricular activities club of the
           prestigious Notre Dame College, has started its journey on 2018, to promote and support
           learning and interaction within areas related to information systems and technology, to
@@ -34,37 +70,6 @@ const Sections = [
         </p>
       </>
     ),
-  },
-  {
-    heading: 'Our Motive',
-    content: (
-      <p>
-        Technology, the catalyst of modern human revolution, has rooted its megabytes, gigabytes and
-        terabytes into every neuron and synapse of our human brains, conquering the whole world with
-        its enigma in the process. In this age, where information has become synonymous to power;
-        with technology as its driving force, the need for a club in Notre Dame College for the
-        young tech enthusiasts couldn't be more. You asked for it. We made it happen. Notre Dame
-        Information Technology Club (NDITC), a co-curricular activities club of the prestigious
-        Notre Dame College, has started its journey on 2018, to promote and support learning and
-        interaction within areas related to information systems and technology, to share new ideas
-        and to encourage recreational activities in these areas. "Innovate and Encode Your Ideas"
-        bearing this motto, the club has been established. Being the first club of Notre Dame
-        College devoted to encourage the students to take part and make contribution to the ever
-        evolving IT sector, NDITC is dedicated to set an example. The club aspires to encourage its
-        members to discover their untapped zeal for information and technology. NDITC aims to give
-        the necessary guidelines and opportunity and act as the leading light in spreading
-        technological awareness among the pupils. Information and technology, with their own
-        conundrum, are expected to create students who are up-to-date with the current world, being
-        led by this sincere institution of knowledge. The club has taken the solemn oath to uphold
-        the rich history of Notre Dame College through our activities and ignite the thirst for
-        knowledge about the technology world within the students which, we believe, will construct
-        the pathway towards a technologically advanced nation.
-      </p>
-    ),
-  },
-  {
-    heading: 'Our Achievements',
-    content: <Timeline />,
   },
 ];
 
