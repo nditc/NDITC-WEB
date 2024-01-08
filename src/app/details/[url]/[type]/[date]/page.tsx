1781fb1e9ba7c33423d4cd251a15ec7da8ae3883
The-XENO-Studios <thexenostudios@gmail.com>
2024-01-08 13:06:11 +0600

    Button Hover Animations Updated

--- src/app/details/[url]/[type]/[date]/page.tsx
+++ src/app/details/[url]/[type]/[date]/page.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
-import { MdOutlineDateRange } from 'react-icons/md';
-import { notFound } from 'next/navigation';
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { MdOutlineDateRange } from "react-icons/md";
+import { notFound } from "next/navigation";
 
 type ParamType = { url: string; type: string; date: number };
 
@@ -33,12 +33,14 @@ const Page = async ({ params }: { params: ParamType }) => {
           <div className="container flex flex-col md:flex-row gap-5 items-center ">
             <div className="flex-1 ml-1 flex flex-col gap-5 w-full order-2 md:order-1">
               <Link href={`/activities?type=${params.type}`}>
-                <h1 className="text-2xl pt-5 underline hover:text-blue-500">{params.type}&gt;</h1>
+                <h1 className="text-2xl pt-5 underline hover:text-blue-500">
+                  {params.type}&gt;
+                </h1>
               </Link>
 
               <h1 className="text-4xl  2xl:text-5xl ">{data.title}</h1>
               <p className="line-clamp-5  font-semibold flex justify-start items-center">
-                <MdOutlineDateRange className={'mr-2 w-6 h-6 '} />
+                <MdOutlineDateRange className={"mr-2 w-6 h-6 "} />
 
                 {DateData.toDateString()}
               </p>
@@ -48,18 +50,18 @@ const Page = async ({ params }: { params: ParamType }) => {
                   <a
                     href={data.action.target}
                     target="_blank"
-                    className="Bebas inline-block cursor-pointer text-xl py-2 font-Bebas px-7 font-medium text-whiterounded-lg border focus:z-10 focus:ring-4  focus:ring-gray-700 bg-black text-white border-gray-600 hover:text-white hover:bg-zinc-700 rounded-lg"
+                    className="before:ease relative flex items-center justify-center  overflow-hidden shadow-2xl transition-all before:absolute before:top-1/2 before:h-0 before:w-64 before:origin-center before:-translate-x-20 before:rotate-45 before:bg-blue-500 before:duration-300 hover:shadow-blue-500 hover:before:h-64 hover:before:-translate-y-32 Bebas cursor-pointer text-xl py-2 font-Bebas px-7 font-medium text-whiterounded-lg border focus:z-10 focus:ring-4  focus:ring-gray-700 bg-black text-white border-gray-600 hover:text-white hover:bg-zinc-700 rounded-lg"
                   >
-                    {data.action.label}
+                    <span className="relative z-10">{data.action.label}</span>
                   </a>
                   {params.url ==
-                    'https%3A%2F%2Fnditc.pythonanywhere.com%2Fprojects%2FgAAAAABklDtCA8wgOMZar9YbvFJgn8e-jFUG27Ic9aHwhx40IBCUofeQ_uoinoAgpmskv_ojJNA4I9Rgswp3RLa9wWgp6846Zg%3D%3D' && (
+                    "https%3A%2F%2Fnditc.pythonanywhere.com%2Fprojects%2FgAAAAABklDtCA8wgOMZar9YbvFJgn8e-jFUG27Ic9aHwhx40IBCUofeQ_uoinoAgpmskv_ojJNA4I9Rgswp3RLa9wWgp6846Zg%3D%3D" && (
                     <a
                       href="https://raw.githubusercontent.com/nditc/nditc_mobile_app/main/nditc.apk"
                       target="_blank"
-                      className="self-start Bebas inline-block cursor-pointer text-xl py-2 font-Bebas px-7 font-medium text-whiterounded-lg border-2 focus:z-10 focus:ring-4  focus:ring-gray-700 border-zinc-800 hover:text-white hover:bg-zinc-800 rounded-lg"
+                      className="relative overflow-hidden bg-white shadow-2xl transition-all before:absolute before:left-0 before:top-0 before:h-full before:w-0 before:duration-500 after:absolute after:right-0 after:top-0 after:h-full after:w-0 after:duration-500 hover:shadow-pink-400 hover:before:w-2/4 hover:before:bg-zinc-900 hover:after:w-2/4 hover:after:bg-zinc-900 self-start Bebas inline-block cursor-pointer text-xl py-2 font-Bebas px-7 font-medium text-whiterounded-lg border-2 focus:z-10 focus:ring-4  focus:ring-gray-700 border-zinc-800 hover:text-white rounded-lg"
                     >
-                      Download Our APP
+                      <span className="relative z-10">Download Our APP</span>
                     </a>
                   )}
                 </div>
