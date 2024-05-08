fdab8f043a1611462776bcce3073af220009d0b5
The-XENO-Studios <thexenostudios@gmail.com>
2024-05-08 15:48:56 +0600

    Notification Update

--- src/app/Components/Sidebar/NewNotification.tsx
+++ src/app/Components/Sidebar/NewNotification.tsx
@@ -1,11 +1,14 @@
 import SaveData from "./SaveData";
 
 const NewNotification = async () => {
+  let data = [];
   const res = await fetch(
     "https://nditc.pythonanywhere.com/api/v1/notifications/web?page=1&limit=1",
     { cache: "no-store" }
   );
-  const data = await res.json();
+  if (res.ok) {
+    data = await res.json();
+  }
 
   return <SaveData data={data} />;
 };
