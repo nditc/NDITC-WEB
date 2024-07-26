6244a01f478b77a1bfe5482055a61d38e84456ff
The-XENO-Studios <thexenostudios@gmail.com>
2024-07-26 13:55:07 +0600

    Import Error Fix

--- src/app/Components/Sidebar/SideBar.tsx
+++ src/app/Components/Sidebar/SideBar.tsx
@@ -7,7 +7,6 @@ import { AiFillYoutube } from "react-icons/ai";
 import { FaGithub } from "react-icons/fa";
 import NewNotification from "./NewNotification";
 import { FaCode } from "react-icons/fa6";
-import ErrorBoundary from "./error";
 
 const SideBar = () => {
   return (
