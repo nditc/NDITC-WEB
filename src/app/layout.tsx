22219913d31438e2527b79e27bcb022657ca6a97
The-XENO-Studios <thexenostudios@gmail.com>
2024-08-17 16:40:41 +0600

    Revert "Error Tweak"
    This reverts commit cf11c93989d319d8e77a7b1d694df3183b21e527.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,36 +1,29 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import "./styles/markdown.css";
-import Navbar from "./Components/Navbar";
-import SideBar from "./Components/Sidebar/SideBar";
-import Footer from "./Components/Footer";
-import NextTopLoader from "nextjs-toploader";
-import { Suspense } from "react";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import './styles/markdown.css';
+import Navbar from './Components/Navbar';
+import SideBar from './Components/Sidebar/SideBar';
+import Footer from './Components/Footer';
+import NextTopLoader from 'nextjs-toploader';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "NDITC",
-  description: "Love the light of Technology",
-  icons: { icon: "favicon.ico" },
+  title: 'NDITC',
+  description: 'Love the light of Technology',
+  icons: { icon: 'favicon.ico' },
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>
         <NextTopLoader color="#3b82f6" />
         <Navbar />
-        <Suspense>
-          <div id="scrollToTop" className="w-0 h-0" />
-          <SideBar />
-          {children}
-        </Suspense>
+        <div id="scrollToTop" className="w-0 h-0" />
+        <SideBar />
+        {children}
         <Footer />
       </body>
     </html>
