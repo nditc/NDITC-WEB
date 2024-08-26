69c452bd8fba9ab3eedbbdd8aef5439c3cf9bb9e
RafsanAmin <rafsanamin2020@gmail.com>
2024-08-26 16:07:36 +0600

    fix error and notification

--- src/app/Components/Sidebar/NewNotification.tsx
+++ src/app/Components/Sidebar/NewNotification.tsx
@@ -1,16 +1,23 @@
-import SaveData from "./SaveData";
+import SaveData from './SaveData';
 
 const NewNotification = async () => {
-  let data = [];
-  const res = await fetch(
-    "https://nditc.pythonanywhere.com/api/v1/notifications/web?page=1&limit=1",
-    { cache: "no-store" }
-  );
-  if (res.ok) {
-    data = await res.json();
-  }
+  try {
+    let data = [];
 
-  return <SaveData data={data} />;
+    const res = await fetch(
+      'https://nditc.sspythonanywhere.com/api/v1/notifications/web?page=1&limit=1',
+      { cache: 'no-store' }
+    );
+
+    if (!res.ok) {
+      return <div />;
+    }
+    data = await res.json();
+
+    return <SaveData data={data} />;
+  } catch (err) {
+    return <div />;
+  }
 };
 
 export default NewNotification;
