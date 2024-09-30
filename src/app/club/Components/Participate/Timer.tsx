e051eaf49b726558134d10ee3bd9e3811b38be8d
The-XENO-Studios <thexenostudios@gmail.com>
2024-09-30 21:17:09 +0600

    Not Yet

--- src/app/club/Components/Participate/Timer.tsx
+++ src/app/club/Components/Participate/Timer.tsx
@@ -5,21 +5,37 @@ import { timeValue } from "../Time";
 import { Timestamp } from "firebase/firestore";
 
 const Timer = ({
+  currentTime,
   endTime,
   onEnd,
   submitClicked,
   setSubmitClicked,
 }: {
+  currentTime: number;
   endTime: number;
   onEnd: () => void;
   submitClicked: boolean;
   setSubmitClicked: (value: boolean) => void;
 }) => {
-  const [hours, setHours] = useState(1);
-  const [minutes, setMinutes] = useState(1);
-  const [seconds, setSeconds] = useState(1);
+  const [timeL, setTimeL] = useState(endTime - currentTime);
+  const [clicked, setClicked] = useState(false);
 
-  const calculateTimeLeft = async () => {
+  const calculateTimeLeft = () => {
+    setTimeL((prev) => prev - 1000);
+
+    if (
+      timeValue(Timestamp.fromMillis(timeL)).hour - 6 <= 0 &&
+      timeValue(Timestamp.fromMillis(timeL)).minute <= 0 &&
+      timeValue(Timestamp.fromMillis(timeL)).seconds <= 15 &&
+      !clicked
+    ) {
+      setSubmitClicked(true);
+      setClicked(true);
+      console.log("Hi");
+      onEnd();
+    }
+
+    /*
     const now = await Date.now();
     const timeLeft = endTime - now;
 
@@ -36,34 +52,44 @@ const Timer = ({
       setSubmitClicked(true);
       console.log("Hi");
       onEnd();
-    }
+    }*/
   };
 
   useEffect(() => {
-    setInterval(() => {
-      calculateTimeLeft();
+    const interval = setInterval(() => {
+      if (timeL > 0) {
+        calculateTimeLeft();
+      }
     }, 1000);
+
+    return () => clearInterval(interval);
   }, []);
 
   return (
     <div className="Inter inline-flex h-20 items-center justify-start rounded-xl">
       <div className="flex h-full items-center justify-start gap-1 rounded-xl text-primary">
-        {hours > 0 && (
+        {timeValue(Timestamp.fromMillis(timeL)).hour - 6 > 0 && (
           <>
             <div className="flex flex-col items-center justify-center">
-              <div className="text-3xl font-extrabold">{hours}</div>
+              <div className="text-3xl font-extrabold">
+                {timeValue(Timestamp.fromMillis(timeL)).hour - 6}
+              </div>
               <div className="text-base">Hours</div>
             </div>
             <div className="self-start pt-2 font-extrabold">:</div>
           </>
         )}
         <div className="flex flex-col items-center justify-center">
-          <div className="text-3xl font-extrabold">{minutes}</div>
+          <div className="text-3xl font-extrabold">
+            {timeValue(Timestamp.fromMillis(timeL)).minute}
+          </div>
           <div className="text-base">Minutes</div>
         </div>
         <div className="self-start pt-2 font-extrabold">:</div>
         <div className="flex flex-col items-center justify-center">
-          <div className="text-3xl font-extrabold">{seconds}</div>
+          <div className="text-3xl font-extrabold">
+            {timeValue(Timestamp.fromMillis(timeL)).seconds}
+          </div>
           <div className="text-base">Seconds</div>
         </div>
       </div>
