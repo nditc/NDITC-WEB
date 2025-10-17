51fabf74fbae28560523e48dce0d6e56c22285ea
t4sn33m-s4h4t <mdsahat6397@gmail.com>
2025-10-17 14:37:34 +0600

    added target blank

--- src/app/club/Components/Events/PartcipateButton.tsx
+++ src/app/club/Components/Events/PartcipateButton.tsx
@@ -11,6 +11,7 @@ const PartcipateButton = ({ id, isExternal, externalLink }: { id: string, isExte
 
     <Link
       href={externalLink}
+      target="_blank"
       className="inline-flex items-center justify-center gap-3 rounded-lg bg-primary px-3 py-2 text-center text-base font-medium text-white hover:bg-primary_dark focus:bg-primary_darkest focus:outline-none focus:ring-4"
     >
       <PiStudent className="h-7 w-7" />
